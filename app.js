35a1a1ee60e5a1e48a7bcee2fcb7bcffe2ee1111
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-17 22:39:37 +0200

    settingUp-requiringUserModel-middlware-for-session/saveInformation/-flash/success-error-message-&&-Port

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+
+
+//Requiring user model
+
+dotenv.config({path : './config.env'});
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+});
+
+
+//middleware for session
+app.use(session({
+    secret : 'Just a simple login/sign up application.',
+    resave : true,
+    saveUninitialized : true
+}));
+
+//middleware flash messages
+app.use(flash());
+
+
+
+//setting middlware globally
+app.use((req, res, next)=> {
+    res.locals.success_msg = req.flash(('success_msg'));
+    res.locals.error_msg = req.flash(('error_msg'));
+
+    next();
+});
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.listen(process.env.PORT, ()=> {
+    console.log('Server is running');
+});
