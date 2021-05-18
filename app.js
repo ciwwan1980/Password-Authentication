c95b4204c98380ed4108c777ad969f3d60de4880
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-18 04:10:37 +0200

    adding-response-for-currentUser-as-middlware

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((req, res, next)=> {
     res.locals.success_msg = req.flash(('success_msg'));
     res.locals.error_msg = req.flash(('error_msg'));
     res.locals.error = req.flash(('error'));
-   
+    res.locals.currentUser = req.user;
 
     next();
 });
